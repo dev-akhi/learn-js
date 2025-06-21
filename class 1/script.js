4957fa185cc90575d63f9b66ff53f649fabfadd0
kazirobin <kazirobinking@gmail.com>
2025-06-21 20:52:55 +0600

    class 1

--- class 1/script.js
+++ class 1/script.js
@@ -1,1 +1,21 @@
 console.log("my name Akhi")
+// string "hello"
+// number 3146
+// boolean true/false
+// array ["16546",56465,true,4]
+// object {key:value}
+// var let const
+// nu
+const name1 = "akhi"
+name1 = "akhi1"
+console.log(name1)
+// = is a assign operator
+let a =  "sdfsdf"
+let arr = [ajflk,asdfd,dfj]
+let user =  {
+  name: "akhi",
+  name1: "akhi1",
+  name2: "akhi",
+  name3: "akhi",
+  age: [12,64,67,465]
+}
