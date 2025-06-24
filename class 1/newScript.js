28c0523579c0f05b39aa82598ac3d451efdaa705
Ajmira Parvin AKhi <devakhi2020@gmail.com>
2025-06-24 20:54:31 +0600

    updated 24/06/25

--- class 1/newScript.js
+++ class 1/newScript.js
@@ -1,1 +1,33 @@
-let string = "this is string"
+
+let str = "this is string" ;
+let num = 31456;
+let boolean = true/ false;
+let array = [ "akhi", "fahim", "faizan", 36]
+// console.log(array[2]);
+
+let room = { 
+    
+    items : ["khat","wiredroof","table"],
+
+ }
+let person = {
+    name:"jone", 
+    age:6,
+    job:"freelancer",
+    location:"dhaka"
+} 
+let grandParent ={
+    childer:{
+        name: "dad",
+        children:{
+            name:"me",
+            children:"my baby"
+        }
+    }
+}
+// console.log(grandParent.childer.name)
+
+const  = "akhi"
+// console.log( "1" = 1)
+console.log(typeof "1")
+console.log(typeof 1)
